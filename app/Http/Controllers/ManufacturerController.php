80728c50fc95d1488c30957df2873cede74381fb
Luis <rivasl.works@gmail.com>
2018-03-23 15:32:56 -0300

    Modified controllers of Manufacturer and Vehicle

---
+++ app/Http/Controllers/ManufacturerController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Manufacturer;
+use Illuminate\Http\Request;
+
+class ManufacturerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+//  1     return Manufacturer::all();
+//  2     return response()->json(Manufacturer::all());
+//  3     return response()->json(['data' => Manufacturer::all()]);
+        return response()->json(['data' => Manufacturer::all()], 202);
+    }
+
+    /**]
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return "Creando el fabricante";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $manufacturer = Manufacturer::find($id);
+
+        if ($manufacturer)
+            return response()->json(['data' => $manufacturer], 202);
+        else
+            return response()->json(['msg' => 'Manufacturer '.$id.' not found'], 404);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return "Editando el fabricante con id $id";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return "Actualizando el fabricante con id $id";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return "Eliminando el fabricante con id $id";
+    }
+}
---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Manufacturer;
+use App\Vehicle;
+use Illuminate\Http\Request;
+
+class VehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($manufacturer_id)
+    {
+        $manufacturer = Manufacturer::find($manufacturer_id);
+        $vehicles = $manufacturer->vehicles;
+
+        if ($vehicles)
+            return response()->json(['data' => $vehicles], 202);
+        else
+            return response()->json(['msg' => 'Manufacturer without vehicles'], 404);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($manufacturer_id)
+    {
+        return "Mostrando formulario para crear vehiculo del fabricante $manufacturer_id";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($manufacturer_id, $vehicle_id)
+    {
+        $vehicle = Vehicle::where('manufacturer_id', '=', $manufacturer_id)
+            ->where('id', '=', $vehicle_id)
+            ->limit(1)
+            ->get();
+
+        if ($vehicle)
+            return response()->json(['data' => $vehicle], 202);
+        else
+            return response()->json(['msg' => 'Manufacturer without vehicles'], 404);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($manufacturer_id, $vehicle_id)
+    {
+        return "Editando el vehiculo $vehicle_id del fabricante $manufacturer_id";
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}

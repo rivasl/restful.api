9d98fec590d7baa8faec233f6acbdec800e61f93
Luis <rivasl.works@gmail.com>
2018-03-24 21:26:53 -0300

    Added field 'name'

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -14,15 +14,17 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         user::create([
-            'email' => 'luis@gmail',
+            'email' => 'luis@gmail.com',
+            'name' => 'Luis',
             'password' => \Illuminate\Support\Facades\Hash::make('123'),
         ]);
 
         $faker = Faker::create();
         for ($i = 0; $i < 3; $i++) {
             user::create([
+                'name' => $faker->firstName,
                 'email' => $faker->email(),
-                'password' => $faker->password(),
+                'password' => \Illuminate\Support\Facades\Hash::make($faker->password()),
             ]);
         }
     }

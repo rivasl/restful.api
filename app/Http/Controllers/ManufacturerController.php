36aafaad869b3175691efaf257fa0cd5178ac317
Luis <rivasl.works@gmail.com>
2018-03-27 00:17:39 -0300

    Added method update of manufacturer

--- app/Http/Controllers/ManufacturerController.php
+++ app/Http/Controllers/ManufacturerController.php
@@ -22,7 +22,7 @@ class ManufacturerController extends Controller
 //  1     return Manufacturer::all();
 //  2     return response()->json(Manufacturer::all());
 //  3     return response()->json(['data' => Manufacturer::all()]);
-        return response()->json(['data' => Manufacturer::all()], 202);
+        return response()->json(['data' => Manufacturer::all()], 200);
     }
 
     /**]
@@ -44,11 +44,10 @@ class ManufacturerController extends Controller
     public function store(Request $request)
     {
         if (!$request->get('name') || !$request->get('website'))
-            return response()->json(['msg' => 'Datos incompletos'], 422);
-        else
-        {
+            return response()->json(['msg' => 'Data not completed'], 422);
+        else {
             $manufacturer = Manufacturer::create($request->all());
-            return response()->json(['data' => $manufacturer, 'msg'=>'Manufacturer created'], 202);
+            return response()->json(['data' => $manufacturer, 'msg' => 'Manufacturer created'], 201);
         }
     }
 
@@ -63,7 +62,7 @@ class ManufacturerController extends Controller
         $manufacturer = Manufacturer::find($id);
 
         if ($manufacturer)
-            return response()->json(['data' => $manufacturer], 202);
+            return response()->json(['data' => $manufacturer], 200);
         else
             return response()->json(['msg' => 'Manufacturer ' . $id . ' not found'], 404);
     }
@@ -88,7 +87,41 @@ class ManufacturerController extends Controller
      */
     public function update(Request $request, $id)
     {
-        return "Actualizando el fabricante con id $id";
+        $method = $request->method();
+        $manufacturer = Manufacturer::find($id);
+
+        if ($manufacturer) {
+
+            $name = $request->get('name');
+            $website = $request->get('website');
+
+            /* Method PATCH */
+            if ($method === 'PATCH') {
+                /* Update name */
+                if ($name != null && $name != '') {
+                    $manufacturer->name = $name;
+                }
+
+                /* Update website */
+                if ($website != null && $website != '') {
+                    $manufacturer->website = $website;
+                }
+
+                $manufacturer->save();
+                return response()->json(['msg' => 'Manufacturer \'s record ' . $id . ' edit with PATCH'], 200);
+            }
+
+            /* Method PUT */
+            if (!$name || !$website) {
+                return response()->json(['msg' => 'Data not completed'], 422);
+            } else {
+                $manufacturer->name = $name;
+                $manufacturer->website = $website;
+                $manufacturer->save();
+                return response()->json(['msg' => 'Manufacturer \'s record ' . $id . ' edit with PUT'], 200);
+            }
+        } else
+            return response()->json(['msg' => 'Manufacturer ' . $id . ' not found'], 404);
     }
 
     /**
